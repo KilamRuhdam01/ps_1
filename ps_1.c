73d3741342219ff2e47b9d74d64269854abc3bf4
KilamRuhdam1 <eet202757@iitd.ac.in>
2020-10-13 14:59:34 +0530

    Added Max and Min code

--- ps_1.c
+++ ps_1.c
@@ -86,25 +86,25 @@ int main()
 
     /*! code to find the perfect squares if any across both diagonals */
     
-    
+    printf("Checking the diagonals for perfect squares \n");
     i,j,k =0;
     int l,m,n,p = 0;
 
     for (i = 0; i <=ele ; ++i)
     {
-        printf("Checking the elements for perfect squares");
+        
         k = Arr[i][i];  //main diagonal element
         p=  Arr[i][ele-i-1];  //secondary diagonal element
         for (l=0; l<k;l++)
         {
             if (l*l == k)  //checking for perfect squares
             {
-                printf(" \n The Prime Numbers in main diagonal are : %d ", k);
-                n=1;     //counter to check whether prime numbers exist or not
+                printf(" \n The perfect squares in main diagonal are : %d ", k);
+                n=1;     //counter to check whether perfect squares exist or not
             }
             else if (l*l==p)
             {
-                printf("\n The prime numbers in the other diagonal are : %d ", p);
+                printf("\n The perfect squares in the other diagonal are : %d ", p);
                 n=1;
             }
             
@@ -116,6 +116,29 @@ int main()
         printf("No Number statisfies the criteria");
     }
     
+    /*! code to find the global maxima and minima in the matrix */
+
+    i,j,k=0;
+    i= Arr[0][0];
+    l = Arr [0][0];
+    for ( k = 0; k <= ele; k++)
+    {
+        for ( j = 0; j <=ele; j++)
+        {
+            
+            if ((Arr[k][j]) >= i ) //check whether the number is greater than the first element
+            {
+               i= Arr[k][j];  //change the  number to be checked against
+            }
+            if (Arr[k][j]<=j)
+            {
+                j= Arr[k][j];
+            }   
+        }
+        
+    }
+    printf(" \n The maximum number in the array is : %d ", i);
+    printf(" \n The minimum number in the array is : %d ", j);
     
     return 0;
 }
